7c079b757a3105a9d9af4b201fb232f9c3e7b23d
hlysig <hlysig@gmail.com>
2015-02-15 23:01:08 +0000

    Improving comments.
    Adding function descriptions for the tag service.

--- objectcube/services/base.py
+++ objectcube/services/base.py
@@ -1,40 +1,43 @@
 class BaseTagService(object):
     def get_tags(self, offset=0, limit=10):
         """
-
-        :param offset:
-        :param limit:
-        :return:
+        Fetch tags from data store.
+        :param offset: The offset value for the result.
+        :param limit: The limit value for the result.
+        :return: List of Tag objects.
         """
         raise NotImplementedError()
 
     def add_tag(self, tag):
         """
-
-        :param tag:
-        :return:
+        Adds new tag to data store.
+        :param tag: Tag object filled with data. Note that the id field must
+        be set to None when creating new tag.
+        :return: None
         """
         raise NotImplementedError()
 
     def count(self):
         """
-
-        :return:
+        Returns the count of all tags in data store. Note that this value might
+        not be accurate. This value should be thought of as estimate
+        of the tag count.
+        :return: Integer
         """
         raise NotImplementedError()
 
     def get_by_id(self, _id):
         """
-
-        :param _id:
-        :return:
+        Fetches tag by id.
+        :param _id: Id for a given Tag.
+        :return: Tag object if found, None otherwise
         """
         raise NotImplementedError()
 
     def get_by_value(self, value):
         """
-
-        :param value:
-        :return:
+        Fetches tags by value.
+        :param value: Value to fetch.
+        :return: List of tag objects, or empty list if none are found.
         """
         raise NotImplementedError()
