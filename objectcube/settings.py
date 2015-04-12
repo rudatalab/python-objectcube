3a8920350ccfef9bd4307c1e9c42285735c532fb
hlysig <hlysig@gmail.com>
2015-04-12 20:01:00 +0000

    Minor fixes for jenkins

--- objectcube/settings.py
+++ objectcube/settings.py
@@ -2,10 +2,11 @@ import os
 
 # Database configurations.
 DB_HOST = os.environ.get('OBJECTCUBE_DB_HOST', 'localhost')
-DB_USER = os.environ.get('OBJECTCUBE_DB_USER', os.getlogin())
+DB_USER = os.environ.get('OBJECTCUBE_DB_USER', os.environ.get('LOGNAME'))
 DB_PORT = int(os.environ.get('OBJECTCUBE_DB_PORT', 5432))
-DB_DBNAME = os.environ.get('OBJECTCUBE_DB_NAME', os.getlogin())
-DB_PASSWORD = os.environ.get('OBJECTCUBE_DB_PASSWORD', os.getlogin())
+DB_DBNAME = os.environ.get('OBJECTCUBE_DB_NAME', os.environ.get('LOGNAME'))
+DB_PASSWORD = os.environ.get('OBJECTCUBE_DB_PASSWORD',
+                             os.environ.get('LOGNAME'))
 
 # Concept service configuration.
 FACTORY_CONFIG = {
