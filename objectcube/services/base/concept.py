fb4c5afd6d22df8523c527c84f1a5d67f6a89405
hlysig <hlysig@gmail.com>
2015-04-13 18:32:26 +0000

    Refactoring base to package
    This gives better overivew when working with a single service

---
+++ objectcube/services/base/concept.py
@@ -0,0 +1,75 @@
+class BaseConceptService(object):
+    def count(self):
+        """
+        Returns approximation of number of concepts in data store.
+        :return: Number
+        """
+        raise NotImplementedError()
+
+    def retrieve(self, offset=0, limit=100):
+        """
+        Fetches list of concepts, by offset and limit, in data store.
+        :param offset: Offset value
+        :param limit: Limit value
+        :return: List of concepts.
+        """
+        raise NotImplementedError()
+
+    def add(self, concept):
+        """
+        Adds new concept to data store.
+        :param concept: Concept object.
+        :return: Updated Concept object.
+        """
+        raise NotImplementedError()
+
+    def update(self, concept):
+        """
+        Update concept in data store.
+        :param concept: Concept object. Note that this object must have
+        a valid id.
+        :return: Updated Concept object.
+        """
+        raise NotImplementedError()
+
+    def delete_by_id(self, concept_id):
+        """
+        Delete Concept by id
+        :param concept_id: Number containing concept id.
+        :return: None
+        """
+        raise NotImplementedError()
+
+    def delete(self, concept):
+        """
+        Delete concept object from data store.
+        :param concept: Concept object
+        that will be deleted.
+        :return: None
+        """
+        raise NotImplementedError()
+
+    def retrieve_or_create(self, concept):
+        """
+        Retrieves a given concept if it exists, if not new concept is
+         created and returned.
+        :param concept: Concept object.
+        :return: Concept object.
+        """
+        raise NotImplementedError()
+
+    def retrieve_by_id(self, concept_id):
+        """
+        Fetches Concept by id.
+        :param concept_id: Number
+        :return: Returns Concept by given id if found, otherwise None.
+        """
+        raise NotImplementedError()
+
+    def retrieve_by_title(self, concept_title):
+        """
+        Fetches Concept by id.
+        :param concept_title: Title for a given concept.
+        :return: Returns a Concept if found by a given title, None otherwise.
+        """
+        raise NotImplementedError()
---
+++ objectcube/services/base/object.py
@@ -0,0 +1,50 @@
+from objectcube.factory import get_service
+
+
+class BaseObjectService(object):
+
+    def __init__(self):
+        # TODO (hlysig) inject this into creation of this service.
+        self.blob_service = get_service('BlobService')
+
+    def add(self, stream, name):
+        """
+        Add object to data store.
+        :param stream: Stream to data.
+        :param name: Name of the object. This can be a name that describes
+        the object being added.
+        :return: Object filled with the added object details.
+        """
+        raise NotImplementedError()
+
+    def count(self):
+        """
+        Counts the number of objects in data store. Note that this value
+        might not be accurate
+        :return: Number of objects as number
+        """
+        raise NotImplementedError()
+
+    def add_tags_to_objects(self, objects, tags):
+        """
+        Add tags to objects. All tags in the tags parameter
+        will be applied to all objects in the objects parameter.
+        """
+        raise NotImplementedError()
+
+    def get_objects_by_tags(self, tags):
+        """
+        Fetch objects that have been applied with tags in tags
+        """
+        raise NotImplementedError()
+
+    def get_by_id(self, object_id):
+        """
+        Fetch a given object by id
+        :param object_id: Number representing the value of the object.
+        :return: Object if found, None otherwise.
+        """
+        raise NotImplementedError()
+
+    def get_objects(self, offset=0, limit=10):
+        raise NotImplementedError()
