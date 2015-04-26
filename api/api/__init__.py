a7fd222136170157640ea7c830b41e17ff12c089
hlysig <hlysig@gmail.com>
2015-04-26 11:49:57 +0000

    Working on concept service
    Adding meta object output from concept service endpoint
    Adding more tests for concept service

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,48 +1,18 @@
-import json
-
 from flask import Flask
+from flask import render_template
 from flask.ext import restful
-from flask import request
-
-from objectcube.vo import Concept
-from objectcube.factory import get_service
+from resources import ConceptsResourceList
 
 app = Flask(__name__)
 api = restful.Api(app)
-
-
-class ConceptsResourceList(restful.Resource):
-    def __init__(self, *args, **kwargs):
-        super(ConceptsResourceList, self).__init__(*args, **kwargs)
-        self.concept_service = get_service('ConceptService')
-
-    def get(self):
-        limit = long(request.args.get('limit', 100))
-        offset = long(request.args.get('offset', 0))
-        tags = [t.to_dict() for
-                t in self.concept_service.retrieve(limit=limit, offset=offset)]
-        return tags
-
-    def post(self):
-        data = json.loads(request.data)
-        title = data.get('title')
-        description = data.get('description', '')
-
-        if not title:
-            return 'Concept must have a title', 400
-        try:
-            concept = self.concept_service.add(Concept(title=title,
-                                                       description=description))
-        except Exception as ex:
-            return ex.message, 401
-        return concept.to_dict(), 201
 
 api.add_resource(ConceptsResourceList, '/concepts')
 
 
 @app.route('/')
 def index():
-    return 'o3'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- api/test/test_concept_service.py
+++ api/test/test_concept_service.py
@@ -9,6 +9,7 @@ class TestDatabaseAwareTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestDatabaseAwareTest, self).__init__(*args, **kwargs)
 
+    # XXX: move to some base class.
     def setUp(self):
         with open('schema.sql') as fd:
             data = ''.join(fd.readlines())
@@ -24,11 +25,17 @@ class TestConceptServiceAPI(TestDatabaseAwareTest):
         self.base_url = '/concepts'
         self.app = app.test_client()
 
+    # XXX: Move to some base class.
     def _post_data(self, url, data):
         headers = [('Content-Type', 'application/json')]
         json_data = json.dumps(data)
         headers.append(('Content-Length', json_data))
         return self.app.post(url, headers=headers, data=json_data)
+
+    def _get_data(self, url):
+        # headers = [('Content-Type', 'application/json')]
+        # headers.append(('Content-Length', json_data))
+        return self.app.get(url)
 
     def test_get_concepts_route_exists(self):
         res = self.app.get(self.base_url)
@@ -60,3 +67,42 @@ class TestConceptServiceAPI(TestDatabaseAwareTest):
         self.assertEqual(return_concept.get('title'), data.get('title'))
         self.assertEqual(return_concept.get('description'),
                          data.get('description'))
+
+    def test_response_object(self):
+        number_of_concepts = 1
+        for i in range(number_of_concepts):
+            data = {
+                'description': 'description-{}'.format(i),
+                'title': 'concept-{}'.format(i)
+            }
+            res = self._post_data(self.base_url, data=data)
+            self.assertEquals(res.status_code, 201)
+
+        data = json.loads(self._get_data(self.base_url).data)
+        self.assertTrue(data.get('meta', False))
+        self.assertTrue(data.get('concepts', False))
+        self.assertEquals(number_of_concepts, len(data.get('concepts')))
+
+    def test_pagination(self):
+        number_of_concepts = 1500
+        for i in range(number_of_concepts):
+            data = {
+                'description': 'description-{}'.format(i),
+                'title': 'concept-{}'.format(i)
+            }
+            res = self._post_data(self.base_url, data=data)
+            self.assertEquals(res.status_code, 201)
+
+        for offset in [0, 4, 6]:
+            for limit in [0, 7, 10, 100, 235]:
+                url = self.base_url + '?limit={}&offset={}'.format(
+                    limit, offset
+                )
+                data = json.loads(self._get_data(url).data)
+
+                self.assertEquals(limit, len(data.get('concepts')))
+
+                for i, r in enumerate(data.get('concepts')):
+                    self.assertEquals(r.get('title'), 'concept-{}'.format(
+                        i + offset)
+                    )
