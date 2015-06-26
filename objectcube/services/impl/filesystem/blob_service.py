b717481d62495f703e8264bab387504c9c7a163c
Björn Þór Jónsson <bjorn@ru.is>
2015-06-26 10:05:41 +0200

    Fixed logging to match other objects.

--- objectcube/services/impl/filesystem/blob_service.py
+++ objectcube/services/impl/filesystem/blob_service.py
@@ -1,5 +1,4 @@
 import os
-import logging
 import cStringIO
 import json
 import shutil
@@ -7,23 +6,24 @@ import shutil
 from objectcube.services.base import BaseBlobService
 from objectcube.exceptions import ObjectCubeException
 from objectcube.utils import md5_from_stream
-
-logger = logging.getLogger('FileBlobService')
+from logging import getLogger
 
 READ_CHUNK_SIZE = 512
 
 
 class FileBlobService(BaseBlobService):
-    def flush(self):
-        logging.debug('Calling flush')
-        if os.path.exists(os.path.join(self.blob_disk_location)):
-            shutil.rmtree(self.blob_disk_location)
-        os.makedirs(self.blob_disk_location)
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
+        super(FileBlobService, self).__init__()
+        self.logger = getLogger('FileBlobService')
         self.blob_disk_location = os.environ.get('FILESYSTEM_BLOB_DIR',
                                                  'blobs')
         self.flush()
+
+    def flush(self):
+        self.logger.debug('flush()')
+        if os.path.exists(os.path.join(self.blob_disk_location)):
+            shutil.rmtree(self.blob_disk_location)
+        os.makedirs(self.blob_disk_location)
 
     def _get_blob_path(self, digest):
         return os.path.join(self.blob_disk_location, digest)
@@ -33,32 +33,33 @@ class FileBlobService(BaseBlobService):
                             .format(digest, 'meta'))
 
     def retrieve_uri(self, digest):
-        logging.debug('Calling retrieve_uri')
+        self.logger.debug('has(): %s', repr(digest))
         if self.has(digest):
             return 'file://{}'.format(self._get_blob_path(digest))
 
-        logging.error('No blob with digest found {}'.format(digest))
-        raise ObjectCubeException('No blob found by digest {}'.format(digest))
+        raise ObjectCubeException('No blob found by digest')
 
     def retrieve_meta(self, digest):
+        self.logger.debug('retrieve_meta(): %s', repr(digest))
         if not self.has(digest):
             raise ObjectCubeException('No blob found by digest {}'
                                       .format(digest))
 
         # Check if we have meta file on disk
-        meta_filepath = self._get_meta_location(digest)
+        meta_file_path = self._get_meta_location(digest)
 
         # If not meta has been added, we return empty dict
-        if not os.path.exists(meta_filepath):
+        if not os.path.exists(meta_file_path):
             return dict()
-        with open(meta_filepath, 'r') as fs:
+        with open(meta_file_path, 'r') as fs:
             data = fs.read().strip()
             d = json.loads(data)
             return d
 
     def get_data(self, digest):
+        self.logger.debug('get_data(): %s', repr(digest))
         if not self.has(digest):
-            raise ObjectCubeException('waaa')
+            raise ObjectCubeException('Function requires valid digest')
 
         blob_path = self._get_blob_path(digest)
 
@@ -67,6 +68,8 @@ class FileBlobService(BaseBlobService):
             return data
 
     def add(self, stream, digest=None, meta=None):
+        self.logger.debug('add(): %s / %s / %s',
+                          repr(stream), repr(digest), repr(meta))
         if not digest:
             digest = md5_from_stream(stream)
 
@@ -82,10 +85,11 @@ class FileBlobService(BaseBlobService):
                 data = stream.read(READ_CHUNK_SIZE)
 
         if meta:
-            meta_filepath = self._get_meta_location(digest)
-            with open(meta_filepath, 'w') as fs:
+            meta_file_path = self._get_meta_location(digest)
+            with open(meta_file_path, 'w') as fs:
                 fs.write(json.dumps(meta))
         return digest
 
     def has(self, digest):
+        self.logger.debug('has(): %s', repr(digest))
         return os.path.exists(os.path.join(self.blob_disk_location, digest))
